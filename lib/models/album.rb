adb876492f9e4422d3052bd40f7f32c05579aa0e
denimcouch <alexmata92@ymail.com>
2020-09-08 15:54:36 -0500

    deleted music_library.rb

--- lib/models/album.rb
+++ lib/models/album.rb
@@ -1,3 +1,5 @@
 class Album < ActiveRecord::Base
-
+    has_many :songs
+    has_one :artist, through: :song
+    has_one :genre, through: :song
 end
--- lib/models/music_library.rb
+++
@@ -1,3 +0,0 @@
-class MusicLibrary < ActiveRecord::Base
-
-end
