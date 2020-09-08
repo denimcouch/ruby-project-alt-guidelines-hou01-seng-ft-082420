17e6bd6943d11b3fac86601632d80ec2234bb8c1
denimcouch <alexmata92@ymail.com>
2020-09-08 16:56:56 -0500

    populated seeds.rb with test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,30 @@
 Artist.delete_all
 Song.delete_all
 Genre.delete_all
+Album.delete_all
 #User.delete_all
 
-drake = Artist.create(name: "Drake")
-sonny_and_cher = Artist.create(name: "Sonny and Cher")
+drake = Artist.create(name: "Drake", location: "Los Angeles")
+equip = Artist.create(name: "Equip", location: "Chicago")
 adele = Artist.create(name: "Adele", location: "England")
+kendrick = Artist.create(name: "Kendrick Lamar", location:"Compton, LA")
+
 
 pop = Genre.create(name: "Pop")
 rap = Genre.create(name: "Rap")
+vaporwave = Genre.create(name: "Vaporwave")
+
+good_kid = Album.create(name: "Good Kid, maad City", artist: kendrick)
+cursebreaker = Album.create(name: "Cursebreaker X", artist: equip)
+twenty_five = Album.create(name: "25", artist: adele)
+nineteen = Album.create(name: "19", artist: adele)
 
 hotline_bling = Song.create(name:"Hotline Bling", artist: drake, genre: rap)
-i_got_you = Song.create(name: "I Got You")
-hello = Song.create(name: "Hello", artist: adele, genre: pop)
-under_the_bridge = Song.create(name: "Under the Bridge", artist: adele, genre: pop)
-daydreamer = Song.create(name: "Daydreamer", artist: adele, genre: pop)
+shop = Song.create(name: "Shop Inventory", artist: equip, album: cursebreaker, genre: vaporwave)
+shadow_dancer = Song.create(name: "Shadow Dancer", artist: equip, album: cursebreaker, genre: vaporwave)
+hello = Song.create(name: "Hello", artist: adele, genre: pop, album: twenty_five)
+under_the_bridge = Song.create(name: "Under the Bridge", artist: adele, genre: pop, album: twenty_five)
+daydreamer = Song.create(name: "Daydreamer", artist: adele, genre: pop, album: nineteen)
 
 
 # alex = User.create(user_name: "Alex", location: "Houston")
